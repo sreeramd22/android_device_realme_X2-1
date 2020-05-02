a6b9c03b780626c399c57214c7eb2806834a8536
Harshit Jain <god@harsh1998.me>
2020-05-02 20:32:04 +0530

    x2: fod: move fp press path to OnPress

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -100,7 +100,15 @@ Return<void> FingerprintInscreen::switchHbm(bool enabled) {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    mFingerPressed = true;
     LOG(INFO) << __func__ << " start";
+    set(DIMLAYER_PATH, FP_BEGIN);
+    std::thread([this]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(60));
+            LOG(INFO) << "SETTING FP PRESS PATH";
+            set(FP_PRESS_PATH, FP_BEGIN);
+    }).detach();
+    LOG(INFO) << __func__ <<" exit";
     return Void();
 }
 
@@ -114,17 +122,6 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
-    mFingerPressed = true;
-    LOG(INFO) << __func__ << " start";
-    set(DIMLAYER_PATH, FP_BEGIN);
-    std::thread([this]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(23));
-        if (mFingerPressed) {
-            LOG(INFO) << "SETTING FP PRESS PATH"
-            set(FP_PRESS_PATH, FP_BEGIN);
-        }
-    }).detach();
-    LOG(INFO) << __func__ <<" exit";
     return Void();
 }
 
